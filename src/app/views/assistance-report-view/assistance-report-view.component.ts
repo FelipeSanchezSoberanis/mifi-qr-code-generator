25f289f338310a94d199a94cc04a22cbe5efe654
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-12-16 11:57:30 -0600

    Save sessions in query params

--- src/app/views/assistance-report-view/assistance-report-view.component.ts
+++ src/app/views/assistance-report-view/assistance-report-view.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from "@angular/core";
 import { FormBuilder, FormControl, Validators } from "@angular/forms";
 import { DateTime } from "luxon";
 import { QrCodeData } from "../generate-qr-code-view/generate-qr-code-view.component";
+import { ActivatedRoute, Router } from "@angular/router";
+import { filter } from "rxjs";
 
 export type StudentRegistration = QrCodeData & { registrationTime: string };
 export type AssistanceRecord = { [key: string]: boolean };
@@ -14,12 +16,50 @@ export type AssistanceReport = { [key: string]: AssistanceRecord };
 })
 export class AssistanceReportViewComponent {
   private formBuilder = inject(FormBuilder);
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
 
   sessions = this.formBuilder.array([]);
 
-  addSession = () => this.sessions.push(new FormControl("", [Validators.required]));
+  addSession = () => {
+    const sessions = this.sessions.value as string[];
+    sessions.push("");
+    this.router.navigate(["."], {
+      queryParams: { sessions: JSON.stringify(sessions) },
+      relativeTo: this.activatedRoute
+    });
+  };
 
-  deleteSession = (i: number) => this.sessions.controls.splice(i, 1);
+  deleteSession = (i: number) => {
+    const sessions = this.sessions.value as string[];
+    sessions.splice(i, 1);
+    this.router.navigate(["."], {
+      queryParams: { sessions: JSON.stringify(sessions) },
+      relativeTo: this.activatedRoute
+    });
+  };
+
+  handleSessionChanged = () => {
+    this.router.navigate(["."], {
+      queryParams: { sessions: JSON.stringify(this.sessions.value) },
+      relativeTo: this.activatedRoute
+    });
+  };
+
+  constructor() {
+    this.activatedRoute.queryParams
+      .pipe(
+        filter((queryParams): queryParams is { sessions: string } => {
+          return queryParams["sessions"] !== undefined;
+        })
+      )
+      .subscribe((queryParams) => {
+        const sessions = JSON.parse(queryParams.sessions) as string[];
+        this.sessions.clear();
+        sessions.forEach((s) => this.sessions.push(new FormControl(s, [Validators.required])));
+        this.sessions.updateValueAndValidity();
+      });
+  }
 
   generateAssistanceReport = (
     sessions: DateTime[],
